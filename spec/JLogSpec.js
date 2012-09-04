ff7e8f233067c369e77486ec0735c7e209a79cc3
AlexDeLaCreme <lyosha@gmail.com>
2012-09-04 12:10:38 +0300

    Added more Jasmine tests

--- spec/JLogSpec.js
+++ spec/JLogSpec.js
@@ -44,4 +44,85 @@ describe("JLog", function() {
     logger.setLevel('warn');
     expect(logger.getLevel()).toBe(JLog.WARN);
   });
+
+  it('should be able to get the list of appenders', function() {
+    expect(Object.prototype.toString.call(logger.getAppenders())).toBe('[object Array]');
+  });
+
+  it('should only have ConsoleAppender by default', function() {
+    expect(logger.getAppenders().length).toBe(1);
+    expect(logger.getAppenders()[0].name).toEqual('ConsoleAppender');
+  });
+
+  it('should be able to add appender', function() {
+    expect(logger.getAppenders().length).toBe(1);
+    var newAppender = new JLog.ConsoleAppender;
+    newAppender.name = 'NewAppender';
+    logger.addAppender(newAppender);
+    expect(logger.getAppenders().length).toBe(2);
+    expect(logger.getAppenders()[1].name).toEqual('NewAppender');
+  });
+
+  it('should be able to remove appender', function() {
+    expect(logger.getAppenders().length).toBe(1);
+    logger.removeAppender('ConsoleAppender');
+    expect(logger.getAppenders().length).toBe(0);
+  });
+
+  it('should be calling log() on all available appenders', function() {
+    var consoleAppender1 = logger.getAppender('ConsoleAppender'),
+        consoleAppender2 = new JLog.ConsoleAppender,
+        consoleAppender3 = new JLog.ConsoleAppender,
+        testString = 'This is test warning string';
+
+    consoleAppender2.name = 'ConsoleAppender2';
+    consoleAppender3.name = 'ConsoleAppender3';
+    logger.addAppender(consoleAppender2);
+    logger.addAppender(consoleAppender3);
+
+    spyOn(consoleAppender1, 'log');
+    spyOn(consoleAppender2, 'log');
+    spyOn(consoleAppender3, 'log');
+
+    logger.warn(testString);
+    expect(consoleAppender1.log).toHaveBeenCalled();
+    expect(consoleAppender2.log).toHaveBeenCalled();
+    expect(consoleAppender3.log).toHaveBeenCalled();
+  });
+
+  it('should not be logging when turned off', function() {
+    var appender = logger.getAppender('ConsoleAppender');
+
+    spyOn(appender, 'log');
+
+    logger.turnOff();
+    logger.warn('some string, whatever');
+
+    expect(appender.log.calls.length).toBe(0);
+  });
+
+  it('should only log at or below the set level', function() {
+    var appender = logger.getAppender('ConsoleAppender');
+
+    spyOn(appender, 'log');
+    logger.setLevel('warn');
+
+    logger.debug('something');
+    logger.info('something');
+    logger.warn('something');
+    logger.error('something');
+    logger.fatal('something');
+
+    expect(appender.log.calls.length).toBe(3);
+
+    logger.setLevel('fatal');
+
+    logger.debug('something');
+    logger.info('something');
+    logger.warn('something');
+    logger.error('something');
+    logger.fatal('something');
+
+    expect(appender.log.calls.length).toBe(4);
+  });
 });
